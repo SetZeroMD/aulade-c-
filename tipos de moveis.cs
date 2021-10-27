aee4a097fc2453ed3ace6dd070f0559ad9625f5d
SetZeroMD <83068741+SetZeroMD@users.noreply.github.com>
2021-10-27 20:25:42 -0300

    Add files via upload

---
+++ tipos de moveis.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace loja_de_moveis
+{
+    class tipos_de_moveis 
+    {
+        public string cor;
+        public string solteiro;
+        public string casal;
+        public string cozinha;
+        public string quarto;
+        public string sala_de_estar;
+        public double tamanho;
+        public string tipo_de_material;
+    }
+}
